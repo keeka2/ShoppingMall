7af2f3957b6820aae3c759e3bdfb38a072f1f39b
keeka2 <keeka2@naver.com>
2020-10-29 22:28:05 +0900

    Delete Api_Action.java

--- Shopping/src/market/api/Api_Action.java
+++
@@ -1,8 +0,0 @@
-package market.api;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface Api_Action {
-	String execute(HttpServletRequest request, HttpServletResponse response);
-}
